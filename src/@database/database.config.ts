e906a6b4d0f1f05f0ab4412ebd0e4f30e86d7c39
mrcell0501 <mrcell0501@gmail.com>
2024-04-25 22:42:22 +0200

    feat(environment variables): add .env.example and set environment variables config

--- src/@database/database.config.ts
+++ src/@database/database.config.ts
@@ -6,11 +6,11 @@ dotenvConfig();
 
 const config: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'serstock',
+  host: process.env.DATABASE_HOST ?? '',
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/@database/entities/*.entity.js'],
   migrations: ['dist/@database/migrations/*{.ts,.js}'],
   synchronize: true,
